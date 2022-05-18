c47da7ac917e86a167bcfba45a26596de5113535
Joe Park <sunghun.josephp@gmail.com>
2022-05-17 18:59:38 -0700

    Set up boilerplate for the app listener and root route

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 // Dependencies
-import express from "express";
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res) => {
+  res.send(`AyOO`)
+})
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`We are running on port ${PORT}`)
+})
+
